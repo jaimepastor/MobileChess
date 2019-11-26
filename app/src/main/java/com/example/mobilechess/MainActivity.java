4c6d5dd5585cbc260125229a3ea81df03413463a
Lenard9999 <37104220+Lenard9999@users.noreply.github.com>
2019-11-27 00:28:16 +0800

    Init stuffs

--- app/src/main/java/com/example/mobilechess/MainActivity.java
+++ app/src/main/java/com/example/mobilechess/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mobilechess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    ChessBoard chessBoard;
+    RelativeLayout layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        chessBoard = new ChessBoard(this);
+        layout = new RelativeLayout(this);
+
+        layout.addView(chessBoard);
+        //setContentView(R.layout.activity_main);
+        setContentView(layout);
+    }
+
+    public void startGame(View v){
+        Intent intent = new Intent(this, MainGame.class);
+        startActivity(intent);
     }
 }
