903f589b3d9e9afea7a236e12cadb5908e175a4e
Jaime Pastor <jjaimepastor@gmail.com>
2019-12-09 19:01:06 +0800

    created SQLite Database, connected it to MakeBoardActivity

--- app/src/main/java/com/example/mobilechess/AddPieceActivity.java
+++ app/src/main/java/com/example/mobilechess/AddPieceActivity.java
@@ -3,12 +3,25 @@ package com.example.mobilechess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPieceActivity extends AppCompatActivity {
+
+    EditText name, location, side;
+    Button savePiece;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_piece);
+
+        name = findViewById(R.id.Name);
+        location = findViewById(R.id.Location);
+        side = findViewById(R.id.Side);
     }
+
+
+
+
 }
--- app/src/main/java/com/example/mobilechess/MainActivity.java
+++ app/src/main/java/com/example/mobilechess/MainActivity.java
@@ -7,17 +7,24 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.Square;
 
 public class MainActivity extends AppCompatActivity {
 
     ChessBoard chessBoard;
     RelativeLayout layout;
+    TextView boardPosition;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boardPosition = findViewById(R.id.boardPosition);
 
 //        chessBoard = new ChessBoard(this);
 //        layout = new RelativeLayout(this);
@@ -34,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
     public void makeBoard(View v){
         Intent intent = new Intent (this, MakeBoardActivity.class);
         startActivity(intent);
+    }
+
+    public void test(View v){
+        Board board = new Board();
+        board.clear();
+        board.setPiece(Piece.BLACK_KING, Square.A1);
+
+        boardPosition.setText(board.getFen());
     }
 }
